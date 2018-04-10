8f164592449a624921ec088d2d2988c24cf45586
jhyejun <hyejun417@gmail.com>
2018-04-10 19:39:50 +0900

    이미지 에셋 추가,
    - 이미지 에셋 추가
    - 컬렉션뷰 셀 생성

--- WhenComing/AppDelegate.swift
+++ WhenComing/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = mainStoryboard.instantiateViewController(withIdentifier: "ListSetViewController")
+        
+        self.window?.rootViewController = viewController
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ WhenComing/ListNotSetViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ListNotSetViewController.swift
+//  WhenComing
+//
+//  Created by 장혜준 on 2018. 4. 10..
+//  Copyright © 2018년 장혜준. All rights reserved.
+//
+
+import UIKit
+
+class ListNotSetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
