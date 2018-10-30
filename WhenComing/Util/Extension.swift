941638995064ff5b309420dbd8e4bbb3def56090
jhyejun <hyejun417@gmail.com>
2018-10-30 22:12:39 +0900

    버스 타입 별 텍스트 컬러 정리
    - 버스 타입 별 텍스트 컬러 정리
    (참고 사이트 : https://librewiki.net/wiki/틀:버스_노선색)

--- WhenComing/Util/Extension.swift
+++ WhenComing/Util/Extension.swift
@@ -22,27 +22,27 @@ extension UIColor {
     class func getBusTextColor(busRouteType: String) -> UIColor {
         switch busRouteType {
         case "공항버스":
-            return UIColor.blue
+            return UIColor(red: 0, green: 160, blue: 233, alpha: 1)
         case "마을버스":
-            return UIColor.black
+            return UIColor(red: 83, green: 179, blue: 50, alpha: 1)
         case "간선버스":
-            return UIColor(red: 54, green: 80, blue: 206, alpha: 1)
+            return UIColor(red: 0, green: 104, blue: 183, alpha: 1)
         case "지선버스":
-            return UIColor(red: 86, green: 214, blue: 0, alpha: 1)
+            return UIColor(red: 83, green: 179, blue: 50, alpha: 1)
         case "순환버스":
-            return UIColor.black
+            return UIColor(red: 242, green: 183, blue: 10, alpha: 1)
         case "광역버스":
-            return UIColor(red: 255, green: 0, blue: 0, alpha: 1)
+            return UIColor(red: 230, green: 0, blue: 18, alpha: 1)
         case "인천버스":
-            return UIColor.blue
+            return UIColor(red: 61, green: 91, blue: 171, alpha: 1)
         case "경기버스":
-            return UIColor.blue
+            return UIColor(red: 0, green: 158, blue: 150, alpha: 1)
         case "폐지":
             return UIColor.gray
         case "공용":
-            return UIColor.blue
+            return UIColor(red: 0, green: 104, blue: 183, alpha: 1)
         default: // "버스"
-            return UIColor.blue
+            return UIColor(red: 0, green: 104, blue: 183, alpha: 1)
         }
     }
     
